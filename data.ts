c2712f606bf0b02167b351cc9a47f3a10eac0bce
RajalakshmiJambunathan <ramyajambunathan01@gmail.com>
2026-06-10 16:15:14 +0530

    first commit

---
+++ data.ts
@@ -0,0 +1,11 @@
+
+let firstname:string="Sapthagiri";
+let age:number=24;
+let isTester:boolean=true;
+
+console.log(`Name:${firstname}`);
+console.log(`Age:${age}`);
+console.log(`Tester:${isTester?"Yes":"No"}`);
+
+let user: string = "Playwright Learner";
+console.log(`Welcome ${user} to TypeScript! `); 
---
+++ data1.ts
@@ -0,0 +1,11 @@
+let firstname:string="Ramya"
+let sex:string="female"
+let aadhar=205656348967
+let verification:boolean=true
+
+console.log(`name=${firstname} 01/03/94`)
+console.log(`sex=${sex}`)
+console.log(`aadhar=${aadhar}`)
+console.log(`declaration=${verification}`)
+console.log(`name=${firstname},aadhar=${aadhar}`)
+console.log(firstname+" "+sex)
